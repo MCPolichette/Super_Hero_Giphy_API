4412ee8310d2c791240f1228f392ae5eb2908c61
MCPolichette <mark@polichette.com>
2018-05-22 17:28:21 -0600

    I got it! need to clean up CSS and figure out some of the BONUS stuff.

--- app.js
+++ app.js
@@ -12,36 +12,13 @@ function buttonDisplay() {
     for (i = 0; i < gifArray.length; i++) {
         $("#buttonColumn1").append("<button type='button'  class='pic-button btn btn-success btn-sm'>" + gifArray[i] + " </button>")
     }
-}
-
-$(document).ready(function webLoad() {
-    buttonDisplay();
-    $("#add_button").on("click", function (event) {
-
-        console.log(gifArray)
-        // function that adds button to array, and re-renders the buttons with new button in addition to it
-
-        // Write code to grab the text the user types into the input field
-        var userInput = $("#new_search").val();
-
-        event.preventDefault();
-        gifArray.push(userInput);
-        // The renderButtons function is called, rendering the list of movie buttons
-        console.log(gifArray);
-
-        $(".form-inline")[0].reset();
-        buttonDisplay();
-        webLoad();
-    });
-
+    // Function for pulling up pictures based on button clicked
     $(".pic-button").on("click", function (event) {
-
         console.log("it works?");
         $("#pics_here").empty();
-
         var imageType = $(this).text();
         var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + imageType + "&api_key=xVCRIEMVtbSqDKSYUxx4bcB8UetYmDx2&limit=" + numberOfImages + "&rating=g")
-
+        // Place holder for other filters:  -   
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -49,7 +26,7 @@ $(document).ready(function webLoad() {
             results = response.data;
             console.log(results)
             for (i = 0; i < results.length; i++) {
-                // creating a new div to put in layout
+                // creating a new div to put in layout and appending the rating and images to it
                 var picDiv = $("<div class = 'pic_view'>");
                 var rating = $("<p> Rating: " + results[i].rating + "</p>");
                 var animalImage = $("<img class ='gif'>");
@@ -61,6 +38,7 @@ $(document).ready(function webLoad() {
                 animalImage.attr("class", "gif");
                 $(picDiv).append(rating);
                 $(picDiv).append(animalImage);
+                // appending newly made div to the Html page, and going to next one.
                 $("#pics_here").append(picDiv);
             }
 
@@ -79,6 +57,20 @@ $(document).ready(function webLoad() {
             })
         });
     })
+}
 
-
+$(document).ready(function webLoad() {
+    buttonDisplay();
+    $("#add_button").on("click", function (event) {
+        console.log(gifArray)
+        // function that adds button to array, and re-renders the buttons with new button in addition to it
+        // Write code to grab the text the user types into the input field
+        var userInput = $("#new_search").val();
+        event.preventDefault();
+        gifArray.push(userInput);
+        // The buttonDisplay function is called, rendering the list of buttons
+        console.log(gifArray);
+        $(".form-inline")[0].reset();
+        buttonDisplay();
+    });
 })
