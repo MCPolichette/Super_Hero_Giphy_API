8df158a1781947145064b8530362051698ca8768
MCPolichette <mark@polichette.com>
2018-05-21 21:51:35 -0600

    meh

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Api Key: xVCRIEMVtbSqDKSYUxx4bcB8UetYmDx2
 // have 5 or six buttons ready to go, and a dropdown for total pictures wanted 
-var gifArray = ["cats", "kittens", "dogs", "calfs", "birds", "muppets", "sea turtles", "awww", "rabbit", "hamster", "sugar glider", "frog", "gerbil", "pygmy goat", "chicken"];
+var gifArray = ["cats", "kittens", "dogs", "calfs", "birds", "muppets",
+    "sea turtles", "awww", "rabbit", "hamster", "sugar glider", "frog", "gerbil",
+    "pygmy goat", "chicken"];
 // other global variables:
-var numberOfImages = 5;
+var numberOfImages = 10;
 
 // CURRENT WORKING BUTTON DISPLAY
 function buttonDisplay() {
@@ -12,53 +14,58 @@ function buttonDisplay() {
     }
 }
 
-
 buttonDisplay();
 
-$(".pic-button").on("click", function (event) {
-    console.log("it works?")
-    $("#pics_here").empty();
+$(document).ready(function () {
 
-    var imageType = $(this).text();
-    var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + imageType + "&api_key=xVCRIEMVtbSqDKSYUxx4bcB8UetYmDx2&limit=" + numberOfImages)
+    $(".pic-button").on("click", function (event) {
+        console.log("it works?")
+        $("#pics_here").empty();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        results = response.data;
-        console.log(results)
-        for (i = 0; i < results.length; i++) {
-            // creating a new div to put in layout
-            var picDiv = $("<div class = 'test'>");
-            var rating = $("<p> Rating: " + results[i].rating + "</p>");
-            var animalImage = $("<img class ='gif'>");
-            animalImage.attr("alt", results[i].title);
-            animalImage.attr("src", results[i].images.fixed_height_small_still.url);
-            animalImage.attr("data-still", results[i].images.fixed_height_small_still.url);
-            animalImage.attr("data-move", results[i].images.fixed_height_small.url);
-            animalImage.attr("data-state", "still");
-            $(picDiv).append(rating);
-            $(picDiv).append(animalImage);
-            $("#pics_here").append(picDiv);
+        var imageType = $(this).text();
+        var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + imageType + "&api_key=xVCRIEMVtbSqDKSYUxx4bcB8UetYmDx2&limit=" + numberOfImages)
 
-        }
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            results = response.data;
+            console.log(results)
+            for (i = 0; i < results.length; i++) {
+                // creating a new div to put in layout
+                var picDiv = $("<div class = 'pic_view'>");
+                var rating = $("<p> Rating: " + results[i].rating + "</p>");
+                var animalImage = $("<img class ='gif'>");
+                animalImage.attr("alt", results[i].title);
+                animalImage.attr("src", results[i].images.fixed_height_small_still.url);
+                animalImage.attr("data-still", results[i].images.fixed_height_small_still.url);
+                animalImage.attr("data-move", results[i].images.fixed_height_small.url);
+                animalImage.attr("data-state", "still");
+                animalImage.attr("class", "gif");
+                $(picDiv).append(rating);
+                $(picDiv).append(animalImage);
+                $("#pics_here").append(picDiv);
+            }
+        });
+    })
 
+    $("#add_button").on("click", function (event) {
+        console.log(gifArray)
+        // function that adds button to array, and re-renders the buttons with new button in addition to it
+        event.preventDefault();
+        // Write code to grab the text the user types into the input field
+        var userInput = $("#new_search").val();
+        gifArray.push(userInput);
+        // The renderButtons function is called, rendering the list of movie buttons
+        console.log(gifArray)
+        buttonDisplay();
     });
-})
-$("#add_button").on("click", function (event) {
-    console.log(gifArray)
-    // function that adds button to array, and re-renders the buttons with new button in addition to it
-    event.preventDefault();
-    // Write code to grab the text the user types into the input field
-    var userInput = $("#new_search").val();
-    gifArray.push(userInput);
-    // The renderButtons function is called, rendering the list of movie buttons
-    console.log(gifArray)
-    buttonDisplay();
-});
-$(".test").on("click", function (event) {
+    // $(".gif").on("click", function () {
+    //     console.log("test");
+    // });
+    $(".pic_view").on("click", function () {
 
-    console.log("test");
-    console.log(this)
-});
+        // var state = $(this).attr("data-state")
+        console.log("state")
+    })
+})
