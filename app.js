75dd10e4a6a38aa6b22563e5b50188a65043886c
MCPolichette <mark@polichette.com>
2018-05-23 22:24:34 -0600

    started adding favorite feature... just need to learn the new API for specific ids on Giphy

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Api Key: xVCRIEMVtbSqDKSYUxx4bcB8UetYmDx2
 // have 5 or six buttons ready to go, and a dropdown for total pictures wanted 
-var gifArray = ["cats", "kittens", "dogs", "calfs", "birds", "muppets",
-    "sea turtles", "awww", "rabbit", "hamster", "sugar glider", "frog", "gerbil",
-    "pygmy goat", "chicken"];
+var gifArray = ["Superman", "Batman", "Dr Strange", "Aquaman", "Mr Incredible", "Captain America",
+    "Spiderman", "Avengers", "Antman", "Wonderwoman", "Batgirl", "Justice League", "Marvel",
+    "DC comics", "X-men", "wolverine", "Ironman", "Hulk", "Thor"];
 // other global variables:
 var numberOfImages = 12;
+var favArray = [localStorage.getItem("favoriteHeros")]
+// localStorage.getItem()
 
 // CURRENT WORKING BUTTON DISPLAY
 function buttonDisplay() {
@@ -27,18 +29,23 @@ function buttonDisplay() {
             console.log(results)
             for (i = 0; i < results.length; i++) {
                 // creating a new div to put in layout and appending the rating and images to it
-                var picDiv = $("<div class = 'pic_view'>");
-                var rating = $("<p> Rating: " + results[i].rating + "</p>");
+                var picDiv = $("<div class = ' pic_view'>");
+                var infoDiv = $("<div class = 'info'>")
+                var rating = $("<p class = 'rating'> Rating: " + results[i].rating + "</p>");
                 var animalImage = $("<img class ='gif'>");
+                var favButton = $("<button type ='button' class = 'favButton btn btn-success btn-sm'>Add to Favorites</button>")
                 animalImage.attr("alt", results[i].title);
                 animalImage.attr("src", results[i].images.fixed_height_small_still.url);
                 animalImage.attr("data-still", results[i].images.fixed_height_small_still.url);
                 animalImage.attr("data-move", results[i].images.fixed_height_small.url);
                 animalImage.attr("data-state", "still");
                 animalImage.attr("class", "gif");
-                animalImage.attr("data-id", results.id);
-                $(picDiv).append(rating);
+                favButton.val(results[i].id);
+                $(infoDiv).append(favButton);
+                $(infoDiv).append(rating);
                 $(picDiv).append(animalImage);
+                $(picDiv).append(infoDiv);
+
                 // appending newly made div to the Html page, and going to next one.
                 $("#pics_here").append(picDiv);
             }
@@ -54,12 +61,21 @@ function buttonDisplay() {
                     $(this).attr("src", $(this).attr("data-still"))
                     $(this).attr("data-state", "still")
                 }
+
+            })
+            $(".favButton").on("click", function () {
+                var newFav = $(this).attr("value")
+                favArray.push(newFav);
+
+
+                localStorage.setItem("favoriteHeros", favArray);
             })
         });
     })
 }
 
 $(document).ready(function () {
+
     buttonDisplay();
     $("#add_button").on("click", function (event) {
         console.log(gifArray)
